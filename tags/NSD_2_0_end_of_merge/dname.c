59432c659b99ebfcc1b2eab3299b8455b6e8eb4e
(no author) <(no author)@a26ef69c-88ff-0310-839f-98b793d9c207>
2004-03-25 09:52:26 +0000

    This commit was manufactured by cvs2svn to create tag 'NSD_2_0_end_of_merge'.
    git-svn-id: http://www.nlnetlabs.nl/svn/nsd@1178 a26ef69c-88ff-0310-839f-98b793d9c207

---
+++ tags/NSD_2_0_end_of_merge/dname.c
@@ -0,0 +1,387 @@
+/*
+ * dname.c -- dname operations
+ *
+ * Alexis Yushin, <alexis@nlnetlabs.nl>
+ *
+ * Copyright (c) 2001-2004, NLnet Labs. All rights reserved.
+ *
+ * This software is an open source.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the NLNET LABS nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <config.h>
+
+#include <sys/types.h>
+
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "dns.h"
+#include "dname.h"
+#include "namedb.h"
+#include "util.h"
+
+
+static const uint8_t *strdname (const char *source, const uint8_t *o);
+
+const dname_type *
+dname_make(region_type *region, const uint8_t *name)
+{
+	size_t name_size = 0;
+	uint8_t label_offsets[MAXDOMAINLEN];
+	uint8_t label_count = 0;
+	const uint8_t *label = name;
+	dname_type *result;
+	ssize_t i;
+	
+	assert(name);
+	
+	while (1) {
+		if (label_is_pointer(label))
+			return NULL;
+		
+		label_offsets[label_count] = (uint8_t) (label - name);
+		++label_count;
+		name_size += label_length(label) + 1;
+
+		if (label_is_root(label))
+			break;
+		
+		label = label_next(label);
+	}
+
+	if (name_size > MAXDOMAINLEN)
+		return NULL;
+
+	assert(label_count <= MAXDOMAINLEN / 2 + 1);
+
+	/* Reverse label offsets.  */
+	for (i = 0; i < label_count / 2; ++i) {
+		uint8_t tmp = label_offsets[i];
+		label_offsets[i] = label_offsets[label_count - i - 1];
+		label_offsets[label_count - i - 1] = tmp;
+	}
+
+	result = region_alloc(region, sizeof(dname_type) + (label_count + name_size) * sizeof(uint8_t));
+	result->name_size = name_size;
+	result->label_count = label_count;
+	memcpy((uint8_t *) dname_label_offsets(result),
+	       label_offsets,
+	       label_count * sizeof(uint8_t));
+	memcpy((uint8_t *) dname_name(result),
+	       name,
+	       name_size * sizeof(uint8_t));
+	return result;
+}
+
+
+const dname_type *
+dname_parse(region_type *region, const char *name, const dname_type *origin)
+{
+	uint8_t buf[MAXDOMAINLEN + 1];
+	if (origin) {
+		buf[0] = origin->name_size;
+		memcpy(buf + 1, dname_name(origin), origin->name_size);
+	} else {
+		buf[0] = 1;
+		buf[1] = 0;
+	}
+	return dname_make(region, strdname(name, buf) + 1);
+}
+
+
+const dname_type *
+dname_copy(region_type *region, const dname_type *dname)
+{
+	dname_type *result;
+
+	assert(dname);
+
+	result = region_alloc(region, dname_total_size(dname));
+	result->name_size = dname->name_size;
+	result->label_count = dname->label_count;
+	memcpy((uint8_t *) dname_label_offsets(result),
+	       dname_label_offsets(dname),
+	       result->label_count * sizeof(uint8_t));
+	memcpy((uint8_t *) dname_name(result),
+	       dname_name(dname),
+	       result->name_size * sizeof(uint8_t));
+	
+	return result;
+}
+
+
+const dname_type *
+dname_partial_copy(region_type *region, const dname_type *dname, uint8_t label_count)
+{
+	assert(label_count > 0);
+
+	if (!dname)
+		return NULL;
+	
+	assert(label_count <= dname->label_count);
+
+	return dname_make(region, dname_label(dname, label_count - 1));
+}
+
+
+int
+dname_is_subdomain(const dname_type *left, const dname_type *right)
+{
+	uint8_t i;
+	
+	if (left->label_count < right->label_count)
+		return 0;
+
+	for (i = 1; i < right->label_count; ++i) {
+		if (label_compare(dname_label(left, i),
+				  dname_label(right, i)) != 0)
+			return 0;
+	}
+
+	return 1;
+}
+
+
+int
+dname_compare(const dname_type *left, const dname_type *right)
+{
+	int result;
+	uint8_t label_count;
+	uint8_t i;
+	
+	assert(left);
+	assert(right);
+
+	if (left == right) {
+		return 0;
+	}
+
+	label_count = (left->label_count <= right->label_count
+		       ? left->label_count
+		       : right->label_count);
+
+	/* Skip the root label by starting at label 1.  */
+	for (i = 1; i < label_count; ++i) {
+		result = label_compare(dname_label(left, i),
+				       dname_label(right, i));
+		if (result) {
+			return result;
+		}
+	}
+
+	/* Dname with the fewest labels is "first".  */
+	return (int) left->label_count - (int) right->label_count;
+}
+
+
+int
+label_compare(const uint8_t *left, const uint8_t *right)
+{
+	int left_length;
+	int right_length;
+	size_t size;
+	int result;
+	
+	assert(left);
+	assert(right);
+
+	assert(label_is_normal(left));
+	assert(label_is_normal(right));
+	
+	left_length = label_length(left);
+	right_length = label_length(right);
+	size = left_length < right_length ? left_length : right_length;
+	
+	result = memcmp(label_data(left), label_data(right), size);
+	if (result) {
+		return result;
+	} else {
+		return (int) left_length - (int) right_length;
+	}
+}
+
+
+uint8_t
+dname_label_match_count(const dname_type *left, const dname_type *right)
+{
+	uint8_t i;
+	
+	assert(left);
+	assert(right);
+
+	for (i = 1; i < left->label_count && i < right->label_count; ++i) {
+		if (label_compare(dname_label(left, i),
+				  dname_label(right, i)) != 0)
+		{
+			return i;
+		}
+	}
+
+	return i;
+}
+
+
+const char *
+dname_to_string(const dname_type *dname)
+{
+	return labels_to_string(dname_name(dname));
+}
+
+
+const char *
+labels_to_string(const uint8_t *dname)
+{
+	static char buf[MAXDOMAINLEN + 1];
+	char *p = buf;
+	const uint8_t *label = dname;
+
+	while (!label_is_root(label)) {
+		const uint8_t *data = label_data(label);
+		uint8_t i;
+		for (i = 0; i < label_length(label); ++i) {
+			*p++ = data[i];
+		}
+		*p++ = '.';
+		label = label_next(label);
+	}
+
+	if (buf == p)
+		*p++ = '.';
+	*p++ = '\0';
+	
+	return buf;
+}
+
+
+/*
+ * Parses the string and returns a dname with
+ * the first byte indicating the size of the entire
+ * dname.
+ *
+ * XXX Check if we dont run out of space (p < d + len)
+ * XXX Verify that every label dont exceed MAXLABELLEN
+ * XXX Complain about empty labels (.nlnetlabs..nl)
+ */
+static const uint8_t *
+strdname (const char *source, const uint8_t *o)
+{
+	static uint8_t dname[MAXDOMAINLEN+1];
+
+	const uint8_t *s = (const uint8_t *) source;
+	uint8_t *h;
+	uint8_t *p;
+	uint8_t *d = dname + 1;
+
+	if (*s == '@' && *(s+1) == 0) {
+		for (p = dname, s = o; s < o + *o + 1; p++, s++)
+			*p = DNAME_NORMALIZE(*s);
+	} else {
+		for (h = d, p = h + 1; *s; s++, p++) {
+			switch (*s) {
+			case '.':
+				if (p == (h + 1)) p--;	/* Suppress empty labels */
+				*h = p - h - 1;
+				h = p;
+				break;
+			case '\\':
+				/* Handle escaped characters (RFC1035 5.1) */
+				if ('0' <= s[1] && s[1] <= '9' &&
+				    '0' <= s[2] && s[2] <= '9' &&
+				    '0' <= s[3] && s[3] <= '9')
+				{
+					int val = ((s[1] - '0') * 100 +
+						   (s[2] - '0') * 10 +
+						   (s[3] - '0'));
+					if (val >= 0 && val <= UCHAR_MAX) {
+						s += 3;
+						*p = NAMEDB_NORMALIZE(val);
+					} else {
+						*p = NAMEDB_NORMALIZE(*++s);
+					}
+				} else if (s[1] != '\0') {
+					*p = NAMEDB_NORMALIZE(*++s);
+				}
+				break;
+			default:
+				*p = DNAME_NORMALIZE(*s);
+			}
+		}
+		*h = p - h - 1;
+
+		/* If not absolute, append origin... */
+		if ((*(p-1) != 0) && (o != NULL)) {
+			for (s = o + 1; s < o + *o + 1; p++, s++)
+				*p = DNAME_NORMALIZE(*s);
+		}
+
+		*dname = (uint8_t) (p - d);
+
+	}
+
+	return dname;
+}
+
+
+const dname_type *
+create_dname(region_type *region, const uint8_t *str, const size_t len)
+{
+	uint8_t temp[MAXDOMAINLEN];
+	size_t i;
+
+	assert(len > 0 && len <= MAXLABELLEN);
+
+	temp[0] = len;
+	for (i = 0; i < len; ++i) {
+		temp[i + 1] = DNAME_NORMALIZE(str[i]);
+	}
+	temp[len + 1] = '\000';
+
+	return dname_make(region, temp);
+}
+         
+
+const dname_type *
+cat_dname(region_type *region,
+	  const dname_type *left,
+	  const dname_type *right)
+{
+	uint8_t temp[MAXDOMAINLEN];
+
+	assert(left->name_size + right->name_size - 1 <= MAXDOMAINLEN);
+	
+	memcpy(temp, dname_name(left), left->name_size - 1);
+	memcpy(temp + left->name_size - 1, dname_name(right), right->name_size);
+
+	return dname_make(region, temp);
+}
---
+++ tags/NSD_2_0_end_of_merge/rbtree.h
@@ -0,0 +1,93 @@
+/*
+ * rbtree.h -- generic red-black tree
+ *
+ * Alexis Yushin, <alexis@nlnetlabs.nl>
+ *
+ * Copyright (c) 2001-2004, NLnet Labs. All rights reserved.
+ *
+ * This software is an open source.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the NLNET LABS nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef _RBTREE_H_
+#define	_RBTREE_H_
+
+#include "region-allocator.h"
+
+/*
+ * This structure must be the first member of the data structure in
+ * the rbtree.  This allows easy casting between an rbnode_t and the
+ * user data (poor man's inheritance).
+ */
+typedef struct rbnode_t rbnode_t;
+struct rbnode_t {
+	rbnode_t   *parent;
+	rbnode_t   *left;
+	rbnode_t   *right;
+	int	    color;
+	const void *key;
+};
+
+#define	RBTREE_NULL &rbtree_null_node
+extern	rbnode_t	rbtree_null_node;
+
+typedef struct rbtree_t rbtree_t;
+struct rbtree_t {
+	region_type     *region;
+	
+	/* The root of the red-black tree */
+	rbnode_t	*root;
+
+	/* The number of the nodes in the tree */
+	unsigned long count;
+
+	/* Current node for walks... */
+	rbnode_t	*_node;
+
+	/* Key compare function */
+	int (*cmp) (const void *, const void *);
+};
+
+/* rbtree.c */
+rbtree_t *rbtree_create(region_type *region, int (*cmpf)(const void *, const void *));
+rbnode_t *rbtree_insert(rbtree_t *rbtree, rbnode_t *data);
+rbnode_t *rbtree_search(rbtree_t *rbtree, const void *key);
+int rbtree_find_less_equal(rbtree_t *rbtree, const void *key, rbnode_t **result);
+rbnode_t *rbtree_first(rbtree_t *rbtree);
+rbnode_t *rbtree_last(rbtree_t *rbtree);
+rbnode_t *rbtree_next(rbnode_t *rbtree);
+rbnode_t *rbtree_previous(rbnode_t *rbtree);
+
+#define	RBTREE_WALK(rbtree, k, d) \
+	for((rbtree)->_node = rbtree_first(rbtree);\
+		(rbtree)->_node != RBTREE_NULL && ((k) = (rbtree)->_node->key) && \
+		((d) = (void *) (rbtree)->_node); (rbtree)->_node = rbtree_next((rbtree)->_node))
+
+#endif /* _RBTREE_H_ */
